b9e85025f2e6edb428e6eec5f1ba3a83d4011622
gofl26 <gofl26@naver.com>
2022-05-03 18:40:41 +0900

    base

--- app.js
+++ app.js
@@ -2,22 +2,22 @@ const express = require('express');
 const connect = require('./schemas');
 const cors = require('cors');
 const passportConfig = require('./passport');
-// const fs = require('fs');
-// const http = require('http');
-// const https = require('https');
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 
-// const privateKey = fs.readFileSync(__dirname + '/private.key', 'utf8');
-// const certificate = fs.readFileSync(__dirname + '/certificate.crt', 'utf8');
-// const ca = fs.readFileSync(__dirname + '/ca_bundle.crt', 'utf8');
-// const credentials = {
-//     key: privateKey,
-//     cert: certificate,
-//     ca: ca,
-// };
+const privateKey = fs.readFileSync(__dirname + '/private.key', 'utf8');
+const certificate = fs.readFileSync(__dirname + '/certificate.crt', 'utf8');
+const ca = fs.readFileSync(__dirname + '/ca_bundle.crt', 'utf8');
+const credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca,
+};
 const app_low = express();
 const app = express();
 const httpPort = 3000;
-// const httpsPort = 443;
+const httpsPort = 443;
 
 app_low.use((req, res, next) => {
     if (req.secure) {
@@ -63,9 +63,9 @@ app.get(
     }
 );
 
-// app.listen(httpPort, () => {
-//     console.log(httpPort, '포트로 서버가 켜졌어요!');
-// });
+app.listen(httpPort, () => {
+    console.log(httpPort, '포트로 서버가 켜졌어요!');
+});
 
 app.listen(httpPort, () => {
     console.log('local서버가 켜졌어요!');
@@ -74,9 +74,9 @@ app.listen(httpPort, () => {
 //     console.log('http서버가 켜졌어요!');
 // });
 
-// https.createServer(credentials, app).listen(httpsPort, () => {
-//     console.log('https서버가 켜졌어요!');
-// });
+https.createServer(credentials, app).listen(httpsPort, () => {
+    console.log('https서버가 켜졌어요!');
+});
 
 // const express = require('express');
 // const connect = require('./schemas');

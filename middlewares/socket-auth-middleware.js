df7e4a8c9f2d3e63bf1de24cddd2b962c5e01156
gofl26 <gofl26@naver.com>
2022-05-11 22:22:46 +0900

    소켓 어스미들웨어 추가

---
+++ middlewares/socket-auth-middleware.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const jwt = require('jsonwebtoken');
+const User = require('../schemas/user');
+const Joi = require("joi")
+
+const authorizationSchema = Joi.string().required()
+
+module.exports = async (socket, next) => {
+    try {
+        const Authorization = await authorizationSchema.validateAsync(
+            socket.handshake.headers.authorization)
+        const {userId} = jwt.verify(Authorization, process.env.MY_KEY);
+        await User.findOne({ userId })
+            .exec()
+            .then((user) => {
+                console.log(socket)
+                socket.user = user
+            })
+        next()
+    } catch (error) {
+        console.error(`Socket Error ${socket.handshake.headers.referer} : ${error.message}`);
+        // TODO 클라이언트에게 연결이 실패했다는 것을 알려주는 기능 구현
+        next(new Error(error.message))
+    }
+}

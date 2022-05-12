4bcaab02823abc7a6e86faadca5328878531a8b4
gofl26 <gofl26@naver.com>
2022-05-12 21:58:35 +0900

    chatting 커밋

--- routes/chatting.js
+++ routes/chatting.js
@@ -8,28 +8,11 @@ router.get('/chatting', authMiddleware, async (req, res) => {
     const userId = user.userId;
     //채팅의 마지막내용, 채팅시간, 그사람이 들어가있는 룸들을 다 보내주기, 룸안에 들어있는 유저들의 정보도 같이 보내주기
     try {
-        const chatting = await Room.find(
-            { userId },
-            {
-                postTitle: 1,
-                postDesc: 1,
-                datemate: 1,
-                nickName: 1,
-                userImg: 1,
-                status: 1,
-                maxMember: 1,
-                nowMember: 1,
-                longitude: 1,
-                latitude: 1,
-                createdAt: 1,
-                spot: 1,
-                postCategory: 1,
-                memberAge: 1,
-                memberGender: 1,
-            }
-        ).sort({ $natural: -1 });
+        const chattingRoom = await Room.find({ 
+            $or: [ {userList: userId}, {owner: userId} ] 
+        });
 
-        res.status(200).json({ chatting });
+        res.status(200).json({ chattingRoom });
     } catch (err) {
         console.log(err);
         res.status(400).send({

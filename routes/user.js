49960322e781941bbfa05d316beb29a2a1397e2c
tay yoon <dylanyoon92@gmail.com>
2022-05-20 17:37:05 +0900

    offline

--- routes/user.js
+++ routes/user.js
@@ -38,7 +38,11 @@ router.get('/callback/kakao', kakaoCallback);
 //* 구글로 로그인하기 라우터 ***********************
 router.get(
     '/google',
-    passport.authenticate('google', { scope: ['profile', 'email'] })
+    passport.authenticate('google', {
+        scope: ['profile', 'email'],
+        accessType: 'offline',
+        approvalPrompt: 'force',
+    })
 ); // 프로파일과 이메일 정보를 받는다.
 //? 위에서 구글 서버 로그인이 되면, 네이버 redirect url 설정에 따라 이쪽 라우터로 오게 된다. 인증 코드를 박게됨
 

dfd60a0dfc126bdaacb166731ec1367996341d78
tay yoon <dylanyoon92@gmail.com>
2022-05-21 16:26:54 +0900

    -

--- routes/user.js
+++ routes/user.js
@@ -48,7 +48,23 @@ router.get(
 
 router.get(
     '/google/callback',
-    passport.authenticate('google', { failureRedirect: '/' }), //? 그리고 passport 로그인 전략에 의해 googleStrategy로 가서 구글계정 정보와 DB를 비교해서 회원가입시키거나 로그인 처리하게 한다.
+    passport.authenticate(
+        'google',
+        { failureRedirect: '/' },
+        (err, user, info) => {
+            if (err) return next(err);
+            console.log('콜백~~~');
+            const userInfo = user;
+            const { userId } = user;
+            const token = jwt.sign({ userId }, process.env.MY_KEY);
+
+            result = {
+                token,
+                userInfo,
+            };
+            res.send({ user: result });
+        }
+    ), //? 그리고 passport 로그인 전략에 의해 googleStrategy로 가서 구글계정 정보와 DB를 비교해서 회원가입시키거나 로그인 처리하게 한다.
     (req, res) => {
         res.redirect('/');
     }
